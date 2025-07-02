c03a06dd66e035019888fc36ca4f33c5f2e79c27
rhehdgus8831 <rhehdgus8831@naver.com>
2025-07-02 09:53:04 +0900

    다형성 예제 및 메서드 구현

--- src/chap1_8/poly/drive/Main.java
+++ src/chap1_8/poly/drive/Main.java
@@ -4,16 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car t1 = new Tesla();
-        Car t2 = new Tesla();
-        Car t3 = new Tesla();
+        Tesla t1 = new Tesla();
+        Tesla t2 = new Tesla();
+        Tesla t3 = new Tesla();
 
-        Car s1 = new Sonata();
-        Car s2 = new Sonata();
+        Sonata s1 = new Sonata();
+        Sonata s2 = new Sonata();
 
-        Car m1 = new Mustang();
-        Car m2 = new Mustang();
-        Car m3 = new Mustang();
+        Mustang m1 = new Mustang();
+        Mustang m2 = new Mustang();
+        Mustang m3 = new Mustang();
 
         Car[] cars = {t1, t2, t3, s1, s2, m1, m2, m3, new Sonata()};
         for (Car car : cars){
@@ -23,8 +23,19 @@ public class Main {
         // 결국 다형성을 통해 다른 타입들을 하나의 배열에 담을 수 있음.
         Object[] arr = {1, "dasas", false, new Sonata()};
 
+        System.out.println("======================================= ");
 
+        Driver heartping = new Driver("하츄핑");
 
+        heartping.drive(t1);
+
+        System.out.println("======================================= ");
+
+        CarShop carShop = new CarShop();
+
+        Car car1 = carShop.exportCar(2000);
+        System.out.println("car1 = " + car1);
+        if(car1 != null) car1.accelerate(); // null 체크
 
     }
 }
