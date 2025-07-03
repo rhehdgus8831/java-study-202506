268003f921eda362941e9e8edbe06a45b1c075ad
rhehdgus8831 <rhehdgus8831@naver.com>
2025-07-03 09:58:20 +0900

    열거형 및 싱글턴 패턴 예제클래스 구현

---
+++ src/chap1_9/final_/Main.java
@@ -0,0 +1,14 @@
+package chap1_9.final_;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student st1 = new Student("김철수","990504-1845124");
+        // st1.ssn = "메롱";
+        Student st2 = new Student("박영희", "990704-2114510");
+
+        System.out.println("st1 = " + st1);
+        System.out.println("st2 = " + st2);
+
+    }
+}
