bf3ae3ba238ab228c0d4fcc1c0af9c2082c495f9
rhehdgus8831 <rhehdgus8831@naver.com>
2025-07-10 01:48:02 +0900

    stream API  예제

--- src/chap2_8/practice/q1/Main.java
+++ src/chap2_8/practice/q1/Main.java
@@ -1,6 +1,11 @@
 package chap2_8.practice.q1;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.*;
+import static java.util.stream.Collectors.*;
 
 public class Main {
 
@@ -23,38 +28,98 @@ public class Main {
         // 연습 1: 2021년에 발생한 모든 거래를 찾아 거래액 오름차 정렬(작은 값에서 큰 값).
         // List<Transaction>
 
+       transactions.stream()
+               .filter(t -> t.getYear() == 2021)
+               .sorted((o1, o2) -> o1.getValue() - o2.getValue())
+               .collect(toList())
+               .forEach(System.out::println);
+
+       System.out.println("=============================");
 
 
         // 연습 2: 거래자가 근무하는 모든 도시이름을 중복 없이 나열하시오.
         // List<String>
 
+        transactions.stream()
+                .map(t -> t.getTrader().getCity())
+                .distinct()
+                .collect(toList())
+                .forEach(System.out::println);
 
+        System.out.println("=============================");
 
         // 연습 3: Cambridge에 근무하는 모든 거래자(Trader)를 찾아
         // 거래자리스트로 이름순으로 오름차정렬.
         // List<Trader>
 
+        transactions.stream()
+                .filter(t -> t.getTrader().getCity().equals("Cambridge"))
+                .map(Transaction::getTrader)
+                .distinct()
+                .sorted(comparing(Trader::getName))
+                .collect(toList())
+                .forEach(System.out::println);
+
+        System.out.println("=============================");
 
         // 연습 4: 모든 거래자의 이름을 리스트에 모아서
         // 알파벳순으로  오름차정렬하여 반환
 
+        transactions.stream()
+                .map(transaction -> transaction.getTrader())
+                .sorted(comparing(Trader::getName))
+                .collect(toList())
+                .forEach(System.out::println);
 
 
         // 연습 5: Milan에 거주하는 거래자가 한명이라도 있는지 여부 확인?
 
+        boolean milan = transactions.stream()
+                .anyMatch(t -> t.getTrader().getCity().equals("milan"));
+
+        System.out.println(milan);
+
+        System.out.println("=============================");
 
         // 연습 6: Cambridge에 사는 거래자의 모든 거래액의 총합 출력.
 
+        int cambridgeSuM = transactions.stream()
+                .filter(t -> t.getTrader().getCity().equals("Cambridge"))
+                .mapToInt(t -> t.getValue())
+                .sum();
+        System.out.println(cambridgeSuM);
+
+        System.out.println("=============================");
 
         // 연습 7: 모든 거래에서 최고거래액은 얼마인가?
+
+        transactions.stream()
+                .map(t -> t.getValue())
+                .sorted((o1, o2) -> o2.intValue() - o1.intValue())
+                .limit(1)
+                .collect(toList())
+                .forEach(System.out::println);
+
+        System.out.println("=============================");
 
 
         // 연습 8. 가장 작은 거래액을 가진 거래정보 탐색
 
+        transactions.stream()
+                .map(t -> t.getValue())
+                .sorted((o1, o2) -> o1.intValue() - o2.intValue())
+                .limit(1)
+                .collect(toList())
+                .forEach(System.out::println);
+
+        System.out.println("=============================");
 
 
         // 연습 9. 거래액이 500 이상인 거래들만 필터링하고,
         // 해당 거래의 정보를 출력하시오.
+
+
+
 
 
         // 연습 10. 모든 거래에서 가장 작은 거래액보다
